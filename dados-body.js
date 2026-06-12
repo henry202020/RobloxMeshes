0688e3147bdbfcb334755b389199d2f8a542758b
Henry <henry2012hehe@gmail.com>
2026-06-12 19:45:12 -0300

    Update dados-body.js

--- dados-body.js
+++ dados-body.js
@@ -1,1 +1,28 @@
-
+// Adicionando itens de Body na biblioteca principal
+bibliotecaMeshes.push(
+    {
+        id: 0,
+        nome: "Female Body R6 - Female 1",
+        categoria: ["BODY"], 
+        descricao: "Female 1 - R6",
+        descricaoLonga: "FEMALE - Includes: Torso, Legs, Arms and Torso.",
+        imagens: [
+            "https://i.postimg.cc/sfJL7FFH/Female-Body-R6.png", 
+            "https://i.postimg.cc/MZYLVkkP/Front.png"
+        ],
+        linkDownload: "https://link-center.net/5591385/AFOG538Lfitj",
+        formato: ".ZIP/.mesh",
+        tamanho: "1.58 MB"
+    },
+    {
+        id: 1,
+        nome: "Female Body R6 - Pleasant Ana",
+        categoria: ["BODY"], 
+        descricao: "Pleasant Ana - R6",
+        descricaoLonga: "PLEASANT ANA - Includes: Torso, Legs, Arms and Torso.",
+        imagens: ["https://i.postimg.cc/XYzNYXcB/Female-Body-R6v2-Pleasant-Ana.png"],
+        linkDownload: "https://link-hub.net/5591385/XOBhNwX7PYMy",
+        formato: ".ZIP/.mesh",
+        tamanho: "1.20 MB"
+    }
+);
