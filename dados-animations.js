a1dc2784bf585cabb6318c5b78afeae371481112
Henry <henry2012hehe@gmail.com>
2026-06-13 10:43:49 -0300

    Update dados-animations.js

--- dados-animations.js
+++ dados-animations.js
@@ -34,13 +34,13 @@ bibliotecaMeshes.push(
 bibliotecaMeshes.push(
     {
         id: 12,
-        nome: "Super Hero Idle R6",
+        nome: "Placeholder",
         categoria: ["ANIMATIONS"], 
-        descricao: "Animação customizada de repouso.",
-        descricaoLonga: "Torne seu personagem único com esta postura heróica.",
-        imagens: ["https://via.placeholder.com/500x350?text=Animation+1"],
+        descricao: "?",
+        descricaoLonga: "?",
+        imagens: ["https://i.postimg.cc/Hsz4KtYN/placeholder.jpg"],
         linkDownload: "https://seu-link.com",
         formato: ".rbxm",
-        tamanho: "15 KB"
+        tamanho: "00.00 KB"
     }
 );
