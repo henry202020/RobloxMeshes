5658e2cacae09c3c69eb2d9b1c7b69ade9ce9238
Henry <henry2012hehe@gmail.com>
2026-06-13 10:01:17 -0300

    Add new animations to the library

--- dados-animations.js
+++ dados-animations.js
@@ -2,6 +2,32 @@
 bibliotecaMeshes.push(
     {
         id: 2,
+        nome: "Walk R6 #1",
+        categoria: ["ANIMATIONS"], 
+        descricao: "Walk animation for R6.",
+        descricaoLonga: "Walk animation for R6.",
+        imagens: ["https://streamable.com/k189jt"],
+        linkDownload: "https://seu-link.com",
+        formato: ".rbxm",
+        tamanho: "15 KB"
+    }
+);
+
+    {
+        id: 2,
+        nome: "Super Hero Idle R6",
+        categoria: ["ANIMATIONS"], 
+        descricao: "Animação customizada de repouso.",
+        descricaoLonga: "Torne seu personagem único com esta postura heróica.",
+        imagens: ["https://via.placeholder.com/500x350?text=Animation+1"],
+        linkDownload: "https://seu-link.com",
+        formato: ".rbxm",
+        tamanho: "15 KB"
+    }
+);
+
+    {
+        id: 2,
         nome: "Super Hero Idle R6",
         categoria: ["ANIMATIONS"], 
         descricao: "Animação customizada de repouso.",
